3baef4807163fab3e508f4c63d278e7678685c82
Mariana Rinaldi <rinaldi.mari@gmail.com>
2026-06-23 12:57:20 -0300

    feat: ajuste erro

--- praticas/aluno-online-react/src/layouts/Layout.jsx
+++ praticas/aluno-online-react/src/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Menu from "../components/Menu";
 import useAuthContext from "../contexts/useAuthContext";
 import "./Layout.css";
